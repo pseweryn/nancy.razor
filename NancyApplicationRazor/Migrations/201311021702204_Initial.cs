379f344f159bd4988193e3a5d632ffbb7bbe442e
pseweryn <pawel.seweryn@pl.abb.com>
2013-11-02 18:06:36 +0100

    adding initial migration

---
+++ NancyApplicationRazor/Migrations/201311021702204_Initial.cs
@@ -0,0 +1,27 @@
+namespace NancyApplicationRazor.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Scores",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Points = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Scores");
+        }
+    }
+}
