861d0a6d7a9bbab7b7a616460680375128d4a7d4
pseweryn <pawel.seweryn@pl.abb.com>
2013-11-02 17:25:08 +0100

    Nancy test app with Ajax andEF

---
+++ NancyApplicationRazor/IndexModule.cs
@@ -0,0 +1,29 @@
+﻿namespace NancyApplicationRazor
+{
+    using System.Data.Entity;
+    using Nancy;
+    using Nancy.ModelBinding;
+
+    public class ScoreEntity : DbContext
+    {
+        public string Name { get; set; }
+        public int Score { get; set; }
+    }
+
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters =>
+            {
+                return View["index"];
+            };            
+            
+            Post["/test"] = _ =>
+            {
+                var score = this.Bind<ScoreEntity>();
+                return "Post was a success, the params that were sent are: Name: " + score.Name + ", Score: " + score.Score;
+            };
+        }
+    }
+}
